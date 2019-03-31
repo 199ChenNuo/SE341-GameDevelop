727134d21007d2c0885225bbd2c0c3abeec9c6bb
199ChenNuo <cn.222@sjtu.edu.cn>
2019-03-31 13:46:07 +0800

    shader gui

---
+++ hw3/Assets/Scripts/CustomShaderGUI.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+using System;
+
+enum SpecularChoice
+{
+    True, False
+}
+
+enum ShaderChioce
+{
+    COLOR_ONLY,
+    NORMAL_ONLY,
+    TEX_ONLY,
+    BLINN_PHONG
+}
+
+
+
+public class CustomShaderGUI : ShaderGUI
+{
+    MaterialEditor editor;
+    MaterialProperty[] properties;
+    Material target;
+
+    public override void OnGUI(MaterialEditor editor, MaterialProperty[] properties)
+    {
+        this.editor = editor;
+        this.properties = properties;
+        this.target = editor.target as Material;
+
+        ShaderChioce shaderChioce = ShaderChioce.BLINN_PHONG;
+       
+        if (target.IsKeywordEnabled("USE_COLOR"))
+            shaderChioce = ShaderChioce.COLOR_ONLY;
+        else if (target.IsKeywordEnabled("USE_NORMAL"))
+            shaderChioce = ShaderChioce.NORMAL_ONLY;
+        else if (target.IsKeywordEnabled("USE_TEXTURE"))
+            shaderChioce = ShaderChioce.TEX_ONLY;
+        else
+            shaderChioce = ShaderChioce.BLINN_PHONG;
+  
+        EditorGUI.BeginChangeCheck();
+        shaderChioce = (ShaderChioce)EditorGUILayout.EnumPopup(
+            new GUIContent("Shader Chioce?"), shaderChioce
+        );
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            if (shaderChioce == ShaderChioce.COLOR_ONLY)
+            {
+                
+
+                onColorOnly();
+            }
+            if (shaderChioce == ShaderChioce.NORMAL_ONLY)
+            {
+                target.EnableKeyword("USE_NORMAL");
+                target.DisableKeyword("USE_COLOR");
+                target.DisableKeyword("USE_TEXTURE");
+                target.DisableKeyword("USE_BLINN");
+            }
+            if (shaderChioce == ShaderChioce.TEX_ONLY)
+            {
+                target.EnableKeyword("USE_TEXTURE");
+                target.DisableKeyword("USE_COLOR");
+                target.DisableKeyword("USE_NORMAL");
+                target.DisableKeyword("USE_BLINN");
+
+                onTexOnly();
+            }
+            if (shaderChioce == ShaderChioce.BLINN_PHONG)
+            {
+                target.DisableKeyword("USE_TEXTURE");
+                target.DisableKeyword("USE_COLOR");
+                target.DisableKeyword("USE_NORMAL");
+                target.EnableKeyword("USE_BLINN");
+
+                onBlinn();
+            }
+        }
+    }
+
+    public void onColorOnly()
+    {
+        MaterialProperty mainColor = FindProperty("_MainColor", properties);
+        GUIContent mainColorLaber = new GUIContent(mainColor.displayName);
+        editor.ColorProperty(mainColor, mainColorLaber.text);
+
+        target.EnableKeyword("USE_COLOR");
+        target.DisableKeyword("USE_NORMAL");
+        target.DisableKeyword("USE_TEXTURE");
+        target.DisableKeyword("USE_BLINN");
+    }
+    public void onTexOnly()
+    {
+        MaterialProperty mainTex = FindProperty("_MainTex", properties);
+        GUIContent mainTexLabel = new GUIContent(mainTex.displayName);
+        editor.TextureProperty(mainTex, mainTexLabel.text);
+    }
+
+    public void onBlinn()
+    {
+        MaterialProperty mainTex = FindProperty("_MainTex", properties);
+        GUIContent mainTexLabel = new GUIContent(mainTex.displayName);
+        editor.TextureProperty(mainTex, mainTexLabel.text);
+
+        SpecularChoice specularChoice = SpecularChoice.False;
+        if (target.IsKeywordEnabled("USE_SPECULAR"))
+            specularChoice = SpecularChoice.True;
+        if (specularChoice == SpecularChoice.True)
+        {
+            MaterialProperty shininess = FindProperty("_Shininess", properties);
+            GUIContent shininessLabel = new GUIContent(shininess.displayName);
+            editor.FloatProperty(shininess, "Specular Factor");
+        }
+    }
+}
--- hw3/Assets/Shaders/Final Shader.shader
+++ hw3/Assets/Shaders/Final Shader.shader
@@ -1,10 +1,11 @@
-﻿Shader "Unlit/4.2 Shader"
+﻿Shader "Unlit/Final Shader"
 {
 	Properties{
 		_Color("Color Tint", Color) = (1, 1, 1, 1)
 		_MainTex("Main Tex", 2D) = "white" {}
 		_Specular("Specular", Color) = (1, 1, 1, 1)
 		_Shininess("Shininess", Range(8.0, 256)) = 20
+		_MainColor("Main Color", Color) = (1,1,1,1)
 	}
 		SubShader{
 			Pass {
@@ -14,14 +15,20 @@
 
 				#pragma vertex vert
 				#pragma fragment frag
+				#pragma shader_feature USE_COLOR
+				#pragma shader_feature USE_NORMAL
+				#pragma shader_feature USE_TEXTURE
+				#pragma shader_feature USE_BLINN
 
 				#include "Lighting.cginc"
+			#include "UnityCG.cginc"
 
 				fixed4 _Color;
 				sampler2D _MainTex;
 				float4 _MainTex_ST;
 				fixed4 _Specular;
 				float _Shininess;
+				float4 _MainColor;
 
 				struct a2v {
 					float4 vertex : POSITION;
@@ -37,6 +44,29 @@
 				};
 
 				v2f vert(a2v v) {
+					v2f i;
+#if USE_COLOR
+					
+					//// old version: i.position = mul(UNITY_MATRIX_MVP, v.position);
+					// 位置： 局部坐标系 -> 屏幕空间中的位置
+					i.pos = UnityObjectToClipPos(v.vertex);
+					return i;
+#endif
+#if USE_NORMAL
+					// v2f i;
+					//// old version: i.position = mul(UNITY_MATRIX_MVP, v.position);
+					// 位置： 局部坐标系 -> 屏幕空间中的位置
+					i.pos = UnityObjectToClipPos(v.vertex);
+					// 法线方向：物体空间 -> 世界坐标系
+					i.worldNormal = UnityObjectToWorldNormal(v.normal);
+					return i;
+#endif
+#if USE_TEXTURE
+					i.pos = UnityObjectToClipPos(v.vertex);
+					i.uv = TRANSFORM_TEX(v.texcoord, _MainTex);
+					return i;
+#endif
+#if USE_BLINN
 					v2f o;
 					o.pos = UnityObjectToClipPos(v.vertex);
 
@@ -46,9 +76,23 @@
 
 					o.uv = v.texcoord.xy * _MainTex_ST.xy + _MainTex_ST.zw;
 					return o;
+#endif
+					i.pos = UnityObjectToClipPos(v.vertex);
+					return i;
 				}
 
 				fixed4 frag(v2f i) : SV_Target {
+
+#if USE_COLOR
+					return _MainColor;
+#endif
+#if USE_NORMAL
+					return float4(i.worldNormal, 1);
+#endif
+#if USE_TEXTURE
+					return tex2D(_MainTex, i.uv);
+#endif
+#if USE_BLINN
 					fixed3 worldNormal = normalize(i.worldNormal);
 					fixed3 worldLightDir = normalize(UnityWorldSpaceLightDir(i.worldPos));
 
@@ -64,11 +108,13 @@
 					fixed3 specular = _LightColor0.rgb * _Specular.rgb * pow(max(0, dot(worldNormal, halfDir)), _Shininess);
 
 					return fixed4(ambient + diffuse + specular, 1.0);
+#endif
+					return _MainColor;
 				}
 
 				ENDCG
 			}
 		}
-			FallBack "Specular"
+			// FallBack "Specular"
 			CustomEditor "CustomShaderGUI"
 }
